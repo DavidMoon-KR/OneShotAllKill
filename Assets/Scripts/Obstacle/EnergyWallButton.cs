05ec5d1cb091085ea3c1466856e063b165b99232
kyostone <83689748+kyostone@users.noreply.github.com>
2024-11-11 14:01:03 +0900

    24.11.11
    1. 버튼 상호작용시 색 변경 기능 추가

--- Assets/Scripts/Obstacle/EnergyWallButton.cs
+++ Assets/Scripts/Obstacle/EnergyWallButton.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class EnergyWallButton : MonoBehaviour
@@ -9,13 +10,35 @@ public class EnergyWallButton : MonoBehaviour
 
     // 사운드
     [SerializeField] private AudioClip m_Clip;
+    [SerializeField] private Material m_ChangeColor;
+    private float m_Time = 0.0f;
+    private bool m_ButtonPush = false;
+    private MeshRenderer m_Button;
+    private Material OriginMaterial;
     private AudioSource m_AudioSource;
     private Animator m_Anim;
 
     void Start()
-    {
+    {        
+        m_Button = transform.GetChild(0).GetComponent<MeshRenderer>();
+        OriginMaterial = m_Button.material;
         m_Anim = GetComponent<Animator>();
-        m_AudioSource = GetComponent<AudioSource>();
+        m_AudioSource = GetComponent<AudioSource>();        
+    }
+
+    private void Update()
+    {
+        if (m_ButtonPush)
+            m_Time += Time.deltaTime;
+        if(m_Time > 0.3f)
+        {
+            if (m_Anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+            {
+                ChangeButtonColor();
+                m_ButtonPush = false;
+                m_Time = 0.0f;
+            }                
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -24,6 +47,8 @@ public class EnergyWallButton : MonoBehaviour
         if (collision.collider.tag == "Bullet")
         {
             m_Anim.SetTrigger("Access");
+            m_ButtonPush = true;
+            ChangeButtonColor();
             // 에너지 방벽이 켜있다면 꺼준다.
             if (m_Shield[0].activeSelf)
             {
@@ -43,8 +68,14 @@ public class EnergyWallButton : MonoBehaviour
                 {
                     m_Shield[i].SetActive(true);
                 }
-            }
+            }            
         }
     }
-
+    private void ChangeButtonColor()
+    {
+        if (m_Button.material == OriginMaterial)
+            m_Button.material = m_ChangeColor;
+        else
+            m_Button.material = OriginMaterial;
+    }
 }
