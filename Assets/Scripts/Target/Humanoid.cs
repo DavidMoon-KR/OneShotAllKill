156860c728440c08605b81cde68a93ee06d5701f
kyostone <83689748+kyostone@users.noreply.github.com>
2024-11-22 17:05:08 +0900

    24.11.22
    8스테이지 타겟 유닛 다 죽지 않았는데 클리어 되는 버그 수정

--- Assets/Scripts/Target/Humanoid.cs
+++ Assets/Scripts/Target/Humanoid.cs
@@ -197,7 +197,7 @@ public class Humanoid : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if ((collision.gameObject.CompareTag("Bullet") || collision.gameObject.CompareTag("GasExplosion")) && m_TriggerExplosion == false)
+        if (collision.gameObject.CompareTag("Bullet") && m_TriggerExplosion == false)
         {
             this.GetComponent<BoxCollider>().isTrigger = true;
             StopMoving();
@@ -225,7 +225,7 @@ public class Humanoid : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // 탄과 충돌하거나 가스폭발과 충돌할 경우
-        if ((other.tag == "GasExplosion"))
+        if (other.tag == "GasExplosion" && m_TriggerExplosion == false)
         {
             StopMoving();
             Instantiate(m_Spark, transform.position, Quaternion.identity);
